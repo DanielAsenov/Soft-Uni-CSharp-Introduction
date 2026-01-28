083bcf09813959fee428460c20b35502aff10eb5
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 21:08:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+char symbol = char.Parse(Console.ReadLine());
+
+
+double result = 0;
+
+if (symbol == '+')
+{
+    result = num1 + num2;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine($"{num1} + {num2} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{num1} + {num2} = {result} - odd");
+    }
+}
+else if (symbol == '-')
+{
+    result = num1 - num2;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine($"{num1} - {num2} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{num1} - {num2} = {result} - odd");
+    }
+}
+else if (symbol == '*')
+{
+    result = num1 * num2;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine($"{num1} * {num2} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{num1} * {num2} = {result} - odd");
+    }
+}
+else if (symbol == '/')
+{
+    if (num1 == 0 || num2 == 0)
+    {
+        Console.WriteLine($"Cannot divide {num1} by zero");
+    }
+    else
+    {
+        result = (double)num1 / num2;
+        Console.WriteLine($"{num1} / {num2} = {result:F2}");
+    }
+}
+else if (symbol == '%')
+{
+    if (num1 == 0 || num2 == 0)
+    {
+        Console.WriteLine($"Cannot divide {num1} by zero");
+    }
+    else
+    {
+        result = num1 % num2;
+        Console.WriteLine($"{num1} % {num2} = {result}");
+    }
+}
+
+
