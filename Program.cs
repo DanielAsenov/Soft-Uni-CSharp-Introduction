7c38f030346d6971ac9f7a718e3b4ed73cbb6c2f
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 21:15:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+using Microsoft.VisualBasic;
+
+double age = double.Parse(Console.ReadLine());
+string sex = Console.ReadLine();
+
+if (age < 16 && sex == "m")
+{
+    Console.WriteLine("Master");
+}
+else if (age>=16 && sex == "m")
+{
+    Console.WriteLine("Mr.");
+}
+
+
+if (age < 16 && sex == "f")
+{
+    Console.WriteLine("Miss");
+}
+else if (age >= 16 && sex == "f")
+{
+    Console.WriteLine("Ms.");
+}
