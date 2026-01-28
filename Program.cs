7b3734b6eef54000f993e5f23a4dd7b762e9f017
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 20:25:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+int priceYear = int.Parse(Console.ReadLine());
+
+double shoePrice = priceYear - (priceYear * 0.4);
+double uniformPrice = shoePrice - (shoePrice * 0.2);
+double basketballPrice = uniformPrice * 0.25;
+double accessoriesPrice = basketballPrice * 0.20;
+
+double cost = priceYear + shoePrice + uniformPrice + basketballPrice + accessoriesPrice;
+
+
+Console.WriteLine(cost);
