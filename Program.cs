88235354f3b1fd4f0c1e1bf8db39cff35b2036fa
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 20:19:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+using System.Runtime.CompilerServices;
+
+double deposit = double.Parse(Console.ReadLine());
+int months = int.Parse(Console.ReadLine());
+double interest = double.Parse(Console.ReadLine());
+
+double interestDecimal = interest / 100;
+
+double sum = deposit + months * ((deposit * interestDecimal) / 12);
+
+Console.WriteLine(sum);
