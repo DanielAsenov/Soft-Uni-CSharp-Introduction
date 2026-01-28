4aa5ca53a20288f3ea19ab26d956658906f468b8
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 21:03:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+string movieType = Console.ReadLine();
+int rowsCount = int.Parse(Console.ReadLine());
+int columnsCount = int.Parse(Console.ReadLine());
+double price = 0;
+
+if (movieType == "Premiere")
+{
+    price = rowsCount * columnsCount * 12.00;
+}
+else if (movieType == "Normal")
+{
+    price = rowsCount * columnsCount * 7.50;
+}
+else if (movieType == "Discount")
+{
+    price = rowsCount * columnsCount * 5.00;
+}
+
+Console.WriteLine($"{price:F2}");
