6c61bcfdcdaa9b3cda14dea3586c1bbc040f586c
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 20:40:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+string figure = Console.ReadLine();
+double areaA = double.Parse(Console.ReadLine());
+
+if (figure == "square")
+{
+    double areaSquare = areaA * areaA;
+    Console.WriteLine($"{areaSquare:F3}");
+}
+else if (figure == "rectangle")
+{
+    double areaB = double.Parse(Console.ReadLine());
+    double rectangleArea = areaA * areaB;
+    Console.WriteLine($"{rectangleArea:F3}");
+}
+else if (figure == "circle")
+{
+    double circleArea = (areaA * areaA) * Math.PI;
+    Console.WriteLine($"{circleArea:F3}");
+}
+else if (figure == "triangle")
+{
+    double areaB = double.Parse(Console.ReadLine());
+    double triangleArea = (areaA * areaB) / 2;
+    Console.WriteLine($"{triangleArea:F3}");
+}
