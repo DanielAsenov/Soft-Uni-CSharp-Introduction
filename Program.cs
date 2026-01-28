de5b8dfdfcdf24cce79a78801e0ad1b787e333d3
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 20:58:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+
+
+
+
+double vegetablePricePerKg = double.Parse(Console.ReadLine());
+double fruitKgPricePerKg = double.Parse(Console.ReadLine()); 
+int vegetableKg = int.Parse(Console.ReadLine());
+int fruitKg =   int.Parse(Console.ReadLine());
+
+double vegetablePrice = vegetablePricePerKg * vegetableKg;
+double fruitPrice = fruitKgPricePerKg * fruitKg;
+
+double sum = vegetablePrice + fruitPrice;
+
+double convertSum = sum / 1.94;
+
+Console.WriteLine($"{convertSum:f2}");
