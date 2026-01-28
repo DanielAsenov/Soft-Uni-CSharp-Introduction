c860415efa8d3f11aa7750d9d8b95e815b8fe667
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 20:21:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+
+int pageCount = int.Parse(Console.ReadLine());
+int pages  = int.Parse(Console.ReadLine());
+int days  = int.Parse(Console.ReadLine());
+
+int hoursAll = pageCount / pages;
+int hoursDay = hoursAll / days;
+
+Console.WriteLine(hoursDay);
