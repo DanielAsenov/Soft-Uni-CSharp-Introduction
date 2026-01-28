ba0257be1e5e9d391bbeb9092d86f35e7efdc178
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 20:23:31 +0200

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+
+
+
+
+
+
+int nylon = int.Parse(Console.ReadLine());
+int paint = int.Parse(Console.ReadLine());
+int thinner = int.Parse(Console.ReadLine());
+int hours = int.Parse(Console.ReadLine());
+
+double nylonPrice = (nylon + 2) * 1.50;
+double paintPrice = paint * 14.50 + (paint * 14.50 / 10);
+double thinnerPrice = thinner * 5.00;
+double bagsPrice = 0.40;
+
+double cost = nylonPrice + paintPrice + thinnerPrice + bagsPrice;
+double hoursLabour = (cost * 0.30) * hours;
+double finalPrice = cost + hoursLabour;
+
+Console.WriteLine(finalPrice);
+
