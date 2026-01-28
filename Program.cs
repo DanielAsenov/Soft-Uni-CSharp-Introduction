37b4b80957556e02bbb59934739a99dc15fb94f4
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 20:54:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+string fuelType = Console.ReadLine();
+double fuelCapacity = double.Parse(Console.ReadLine());
+
+if (fuelType != "Diesel" &&  fuelType != "Gasoline" && fuelType != "Gas")
+{
+    Console.WriteLine("Invalid fuel!");
+}
+else if (fuelCapacity < 25)
+{
+    Console.WriteLine($"Fill your tank with {fuelType.ToLower()}!");
+}
+else
+{
+    Console.WriteLine($"You have enough {fuelType.ToLower()}.");
+}   
