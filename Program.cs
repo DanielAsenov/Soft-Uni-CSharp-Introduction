510043bf89a8da351fbb950a99c4e8c5e7ab58e9
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 21:17:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+string dayOfWeek = Console.ReadLine();
+
+int priceOfTicket = 0;
+
+if (dayOfWeek == "Monday" ||  dayOfWeek == "Tuesday" || dayOfWeek == "Friday")
+{
+    priceOfTicket = 12;
+    Console.WriteLine(priceOfTicket);
+}
+else if (dayOfWeek == "Wednesday" || dayOfWeek == "Thursday")
+{
+    priceOfTicket = 14;
+    Console.WriteLine(priceOfTicket);
+}
+else
+{
+    priceOfTicket = 16;
+    Console.WriteLine(priceOfTicket);
+}
