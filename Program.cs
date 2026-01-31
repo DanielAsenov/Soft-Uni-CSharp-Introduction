67055f18ec279983fdf480815c8aeb63be9cc106
Daniel Asenov <theaerie23@gmail.com>
2026-01-31 15:18:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+
+double price = 0;
+string location = "";
+string place = "";
+
+
+if (budget <= 1000)
+{
+    place = "Camp";
+    if (season == "Summer")
+    {
+        location = "Alaska";
+        price = budget * 0.65;
+    }
+    else if (season == "Winter")
+    {
+        location = "Morocco";
+        price = budget * 0.45;
+    }
+}
+else if (budget > 1000 && budget <= 3000)
+{
+    place = "Hut";
+    if (season == "Summer")
+    {
+        location = "Alaska";
+        price = budget * 0.80;
+    }
+    else if (season == "Winter")
+    {
+        location = "Morocco";
+        price = budget * 0.60;
+    }
+}
+else if (budget > 3000)
+{
+    place = "Hotel";
+    if (season == "Summer")
+    {
+        location = "Alaska";
+        price = budget * 0.90;
+    }
+    else if (season == "Winter")
+    {
+        location = "Morocco";
+        price = budget * 0.90;
+    }
+}
+
+Console.WriteLine($"{location} - {place} - {price:F2}");
