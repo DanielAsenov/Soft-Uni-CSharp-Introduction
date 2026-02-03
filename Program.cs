9f7dbf6e907b3fecd1e781992620a2dbf0daebad
Daniel Asenov <theaerie23@gmail.com>
2026-02-03 15:22:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+int age = int.Parse(Console.ReadLine());
+double washingMachinePrice = double.Parse(Console.ReadLine());
+int toyPrice  = int.Parse(Console.ReadLine());
+
+int toyCount = 0;
+int moneySaved = 0;
+int moneyPerEvenBirthday = 10;
+
+
+for (int i = 1;  i <= age; i++)
+{
+    if (i % 2 == 0)
+    {
+        moneySaved += moneyPerEvenBirthday - 1;
+        moneyPerEvenBirthday += 10;
+    }
+    else
+    {
+        toyCount++;
+    }
+}
+
+int totalMoneySaved = moneySaved + (toyCount * toyPrice);
+
+if (totalMoneySaved >= washingMachinePrice)
+{
+    Console.WriteLine($"Yes! {totalMoneySaved - washingMachinePrice:F2}");
+}
+else
+{
+    Console.WriteLine($"No! {washingMachinePrice-totalMoneySaved:F2}");
+}
