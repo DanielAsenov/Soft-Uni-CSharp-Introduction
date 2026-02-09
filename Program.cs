a2d463ffeb85cd706a69ca70f95f2a71872d6f64
Daniel Asenov <theaerie23@gmail.com>
2026-02-09 20:24:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+76
+10
+A
+V
+V
+V
+G
+B
+A
+V
+B
+B
+
+*/
+
+int capacity = int.Parse(Console.ReadLine());
+int numberOfFans = int.Parse(Console.ReadLine());
+
+double sectorA = 0;
+double sectorB = 0;
+double sectorV = 0;
+double sectorG = 0;
+
+
+for (int i = 0; i < numberOfFans; i++)
+{
+    string sector = Console.ReadLine();
+
+    if (sector == "A")
+    {
+        sectorA++;
+    }
+    else if (sector == "B")
+    {
+        sectorB++;
+    }
+    else if (sector == "V")
+    {
+        sectorV++;
+    }
+    else if (sector == "G")
+    {
+        sectorG++;
+    }
+}
+
+Console.WriteLine($"{sectorA / numberOfFans * 100:F2}");
+Console.WriteLine($"{sectorB / numberOfFans * 100:F2}");
+Console.WriteLine($"{sectorV / numberOfFans * 100:F2}");
+Console.WriteLine($"{sectorG / numberOfFans * 100:F2}");
+Console.WriteLine($"{(double)numberOfFans / capacity * 100:F2}");
