bd7a9d7b7c85a0894485a4edd4fed9777866a5f0
Daniel Asenov <theaerie23@gmail.com>
2026-02-10 14:27:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel;
+
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (sum < number)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    sum += num;
+
+
+}
+
+Console.WriteLine(sum);
