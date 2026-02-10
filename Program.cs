8cf47ccc9a6722e565eea53970d5f33737e93ac3
Daniel Asenov <theaerie23@gmail.com>
2026-02-10 14:39:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+string input = Console.ReadLine();
+
+double sum = 0;
+double total = 0;
+
+while (input != "NoMoreMoney")
+{
+    sum = double.Parse(input);
+
+    if (sum < 0)
+    {
+        Console.WriteLine("Invalid operation!");
+        break;
+    }
+
+    total += sum;
+
+    Console.WriteLine($"Increase: {sum:F2}");
+
+    input = Console.ReadLine();
+
+    
+
+}
+
+Console.WriteLine($"Total: {total:F2}");
