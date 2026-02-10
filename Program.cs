324027f2d3c5a0835279b1ae85e4576435e76cf0
Daniel Asenov <theaerie23@gmail.com>
2026-02-10 14:47:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+string input = Console.ReadLine();
+
+int minNumber = int.MaxValue;
+
+
+while (input != "Stop")
+{
+    int number = int.Parse(input);
+
+    if (number < minNumber)
+    {
+        minNumber = number;
+
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine(minNumber);
