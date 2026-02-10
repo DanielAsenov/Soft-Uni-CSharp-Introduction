e398353dfb129b6074707fe67fa2353fce526290
Daniel Asenov <theaerie23@gmail.com>
2026-02-10 15:12:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+
+Gosho
+5
+5.5
+6
+5.43
+5.5
+6
+5.55
+5
+6
+6
+5.43
+
+Mimi
+5
+6
+5
+6
+5
+6
+6
+2
+3
+
+ */
+
+string studentName = Console.ReadLine();
+double studentGrade = double.Parse(Console.ReadLine());
+
+int gradeCount = 0;
+double gradeAverage = 0;
+
+while (studentGrade >= 4.00)
+{
+    gradeAverage += studentGrade;
+    gradeCount++;
+
+    if (gradeCount == 12)
+    {
+        Console.WriteLine($"{studentName} graduated. Average grade: {gradeAverage / gradeCount:F2}");
+        break;
+    }
+
+    studentGrade = double.Parse(Console.ReadLine());
+
+
+}
+
+if (studentGrade < 4)
+{
+    gradeCount++;
+    Console.WriteLine($"{studentName} has been excluded at {gradeCount} grade");
+}
