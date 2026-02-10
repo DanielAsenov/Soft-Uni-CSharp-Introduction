8675bb9b14a02c5da7b4552e7433920efe30756b
Daniel Asenov <theaerie23@gmail.com>
2026-02-10 15:42:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿
+
+/*
+3
+Money
+6
+Story
+4
+Spring Time
+5
+Bus
+6
+Enough
+
+2
+Income
+3
+Game Info
+6
+Best Player
+4
+
+ */
+using System.Runtime.CompilerServices;
+
+int inefficientGrades = int.Parse(Console.ReadLine());
+
+string taskName = Console.ReadLine();
+int grade = int.Parse(Console.ReadLine());
+
+double averageGrade = 0;
+int poorGrades = 0;
+int tasksSolved = 0;
+string lastProblem = "";
+
+
+
+while (taskName != "Enough" || poorGrades >= inefficientGrades)
+{
+    averageGrade += grade;
+    tasksSolved++;
+
+    if (grade <= 4)
+    {
+        poorGrades++;
+        if (poorGrades == inefficientGrades)
+        {
+            Console.WriteLine($"You need a break, {poorGrades} poor grades.");
+            break;
+        }
+    }
+
+    taskName = Console.ReadLine();
+    if (taskName == "Enough")
+    {
+        Console.WriteLine($"Average score: {averageGrade / tasksSolved:F2}");
+        Console.WriteLine($"Number of problems: {tasksSolved}");
+        Console.WriteLine($"Last problem: {lastProblem}");
+        break;
+    }
+    if (taskName != "Enouhg")
+    {
+        lastProblem = taskName;
+    }
+    grade = int.Parse(Console.ReadLine());
+    
+
+}
+
