fca9adc2eb585560834d0bf0febabfe36c088e46
Daniel Asenov <theaerie23@gmail.com>
2026-02-16 19:31:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+int startNumber = int.Parse(Console.ReadLine());
+int lastNumber = int.Parse(Console.ReadLine());
+int reqNumber = int.Parse(Console.ReadLine());
+
+int combinations = 0;
+bool result = false;
+
+
+for (int i = startNumber; i <= lastNumber; i++)
+{
+    for (int j = startNumber; j <=lastNumber; j++)
+    {
+        combinations++;
+        if (i + j == reqNumber)
+        {
+            Console.WriteLine($"Combination N:{combinations} ({i} + {j} = {reqNumber})");
+            result = true;
+            if (result)
+            {
+                break;
+            }
+        }
+        
+        
+    }
+    if (result)
+    {
+        break;
+    }
+}
+
+if (result == false)
+{
+    Console.WriteLine($"{combinations} combinations - neither equals {reqNumber}");
+}
