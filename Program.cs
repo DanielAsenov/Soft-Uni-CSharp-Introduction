4dbfa4986a3567950add246562af6b09b0c06729
Daniel Asenov <theaerie23@gmail.com>
2026-02-16 12:38:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+int number = int.Parse(Console.ReadLine());
+
+int validCombination = 0;
+
+for (int i = 0; i <= number; i++)
+{
+    for (int j = 0; j <= number; j++)
+    {
+        for (int k = 0;  k <= number; k++)
+        {
+            if (i + j + k == number)
+            {
+                validCombination++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(validCombination);
