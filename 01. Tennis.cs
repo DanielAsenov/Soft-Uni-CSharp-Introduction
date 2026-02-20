658d46b0962e4b4c8571a37c52adf12b327c157f
Daniel Asenov <theaerie23@gmail.com>
2026-02-20 13:59:13 +0200

    Add files via upload

---
+++ 01. Tennis.cs
@@ -0,0 +1,20 @@
+﻿
+
+double tennisRacketPrice = double.Parse(Console.ReadLine());
+int tennisRacketAmount = int.Parse(Console.ReadLine());
+int shoeAmount = int.Parse(Console.ReadLine());
+
+double shoePriceAmount = shoeAmount * (tennisRacketPrice / 6);
+double tennisRacketsPrice = tennisRacketPrice * tennisRacketAmount;
+
+double totalPrice = tennisRacketsPrice + shoePriceAmount;
+double otherEquipementPrice = totalPrice * 0.20;
+
+double finalPrice = totalPrice + otherEquipementPrice;
+
+Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(finalPrice / 8)}");
+Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling((finalPrice * 7) / 8)}");
+
+
+
+
